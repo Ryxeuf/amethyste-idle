e710cadeec04e0dad0f4165ea10693d54d1be420
Remy Mandon <163468313+RemyFoodomarket@users.noreply.github.com>
2025-03-06 12:23:01 +0100

    fix monster attack

--- src/DataFixtures/MonsterFixtures.php
+++ src/DataFixtures/MonsterFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use App\Entity\Game\Monster;
 use App\Entity\Game\Spell;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class MonsterFixtures extends Fixture
+class MonsterFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -15,57 +16,51 @@ class MonsterFixtures extends Fixture
         $monsters = [
             'zombie' => [
                 'name' => 'Zombie',
-                'life' => 20,
-                'level' => 1,
+                'life' => 10,
                 'hit' => 80,
-                'speed' => 5
+                'speed' => 2,
+                'attack' => 'none_attack_1',
+                'level' => 1
             ],
             'skeleton' => [
                 'name' => 'Squelette',
-                'life' => 15,
-                'level' => 1,
-                'hit' => 85,
-                'speed' => 8
+                'life' => 20,
+                'hit' => 80,
+                'speed' => 5,
+                'attack' => 'punishment',
+                'level' => 1
             ],
             'ochu' => [
                 'name' => 'Ochu',
-                'life' => 30,
-                'level' => 2,
-                'hit' => 75,
-                'speed' => 3
+                'life' => 20,
+                'hit' => 80,
+                'speed' => 15,
+                'attack' => 'liana_whip',
+                'level' => 2
             ],
             'taiju' => [
                 'name' => 'Taiju',
-                'life' => 40,
-                'level' => 3,
-                'hit' => 70,
-                'speed' => 4
+                'life' => 10,
+                'hit' => 60,
+                'speed' => 12,
+                'attack' => 'liana_whip',
+                'level' => 3
             ]
         ];
-        
-        // Création d'un sort d'attaque de base pour les monstres
-        $basicAttack = new Spell();
-        $basicAttack->setName('Attaque de base');
-        $basicAttack->setSlug('basic-attack');
-        $basicAttack->setDescription('Attaque de base des monstres');
-        $basicAttack->setDamage(3);
-        $basicAttack->setHit(90);
-        $basicAttack->setCritical(5);
-        $basicAttack->setCreatedAt(new \DateTime());
-        $basicAttack->setUpdatedAt(new \DateTime());
-        
-        $manager->persist($basicAttack);
-        $this->addReference('basic_attack', $basicAttack);
         
         foreach ($monsters as $key => $data) {
             $monster = new Monster();
             $monster->setName($data['name']);
             $monster->setSlug($key);
             $monster->setLife($data['life']);
-            $monster->setLevel($data['level']);
             $monster->setHit($data['hit']);
             $monster->setSpeed($data['speed']);
-            $monster->setAttack($basicAttack);
+            $monster->setLevel($data['level']);
+            
+            // Récupération du sort d'attaque
+            $attackSpell = $this->getReference($data['attack'], Spell::class);
+            $monster->setAttack($attackSpell);
+            
             $monster->setCreatedAt(new \DateTime());
             $monster->setUpdatedAt(new \DateTime());
             
@@ -74,5 +69,12 @@ class MonsterFixtures extends Fixture
         }
         
         $manager->flush();
+    }
+    
+    public function getDependencies(): array
+    {
+        return [
+            SpellFixtures::class,
+        ];
     }
 } 
