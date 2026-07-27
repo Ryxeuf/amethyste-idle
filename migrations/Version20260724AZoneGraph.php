8f4c773771d6aaf65b92eb86ffa873b7dc431e47
Debian <debian@packer-output-f2d976ee-0f72-464a-b79c-8cf801e6766b>
2026-07-27 08:41:01 +0000

    fix(zon-03): renommer la migration ZoneGraph pour la trier avant ses dependants
    Version20260724ZoneGraph (creation table zone) se triait apres
    PlayerCurrentZone/WorldEntitiesZone/ZoneGatherVeins par ordre alphabetique
    de version Doctrine, provoquant un crash au deploiement (FK vers une table
    inexistante). Renommage en Version20260724AZoneGraph pour la faire executer
    en premier dans le lot du 20260724.

--- migrations/Version20260724AZoneGraph.php
+++ migrations/Version20260724AZoneGraph.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260724ZoneGraph extends AbstractMigration
+final class Version20260724AZoneGraph extends AbstractMigration
 {
     public function getDescription(): string
     {
